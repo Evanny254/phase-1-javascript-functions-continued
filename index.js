265324b871baf1429e43d2c766090fad0333f07d
Evanny254 <evancherry254@gmail.com>
2023-09-19 17:06:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ function saturdayFun(activity = 'roller-skate') {
     return `This Saturday, I want to ${activity}!`;
   }
   
-  const mondayWork = function(activity = 'go to the office') {
+  function mondayWork(activity = 'go to the office') {
     return `This Monday, I will ${activity}.`;
   };
   
   function wrapAdjective(wrapper = '*') {
-    return function(adjective) {
-      return `${wrapper}${adjective}${wrapper}`;
-    };
+    return function(adjective = "special") {
+      return `You are ${wrapper}${adjective}${wrapper}!`
+    }
   }
-  function wrapAdjective(character) {
-    return function(adjective) {
-      if (character === '*') {
-        return `You are *${adjective}*!`;
-      } else if (character === '||') {
-        return `You are ||${adjective}||!`;
-      } 
-    };
-  }
+ 
